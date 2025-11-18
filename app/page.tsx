19aa13271bcc030e00369dd69bf6fbae42ec7666
evelynngo14 <evelynngo14@gmail.com>
2025-11-18 06:16:52 -0500

    update blog post

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,8 @@ export default function Home() {
           content="
         i coded the retro computer window from scratch, but it took forever to get it looking how i wanted it to!
         i found a few websites that were life-savers for helping me visualize my layouts and understand what i was doing.
+        i got good results using a grid layout with a fixed width and height for each cell using a absolute position.
+        elements like the scroll bar and chevron arrows had to be manually positioned using top / right / bottom / left utilities.
         hopefully once i'm able to optimize the code i'll consider sharing it as a react component."
         />
 
