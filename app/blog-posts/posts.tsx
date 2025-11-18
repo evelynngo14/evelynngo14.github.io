fa2d27fb4e56ee6c00855cbdf36fdc2ac573bf6e
evelynngo14 <evelynngo14@gmail.com>
2025-11-18 13:24:10 -0500

    Update posts.tsx

--- app/blog-posts/posts.tsx
+++ app/blog-posts/posts.tsx
@@ -8,7 +8,7 @@ const post03_md = `
 
 const post01_md = `i coded the retro computer window from scratch, but it took forever to get it looking how i wanted it to! i found a few websites that were life-savers for helping me visualize my layouts and understand what i was doing.  
   &nbsp;  
-  \ni got good results using a grid layout with a fixed width and height for each cell using a absolute position. elements like the scroll bar and chevron arrows had to be manually positioned using top / right / bottom / left utilities. hopefully once i'm able to optimize the code i'll consider sharing it as a react component.`;
+  \ni found good results using a grid layout with a fixed width and height for each cell using absolute positioning. elements like the scroll bar and chevron arrows had to be manually positioned using top / right / bottom / left utilities. hopefully once i'm able to optimize the code i'll consider sharing it as a react component.`;
 
 const post02_md =
   "i'm starting this blog to share my learning journey and exploration into web development. <3";
