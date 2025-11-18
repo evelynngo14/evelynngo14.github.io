7c472e0ce31d2681770bd8f3543f20d8bcea0ced
evelynngo14 <evelynngo14@gmail.com>
2025-11-18 00:01:23 -0500

    Update HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
     <div className="flex min-h-screen">
       <div className="flex flex-col relative items-center justify-center w-full h-full text-border py-3 top-30">
         <RetroWindow>
-          <h1 className="text-lg font-bold p-3 text-center">welcome to my personal site</h1>
+          <h1 className="text-lg text-center font-bold p-3">welcome to my personal site</h1>
           <br></br>
           <div className="text-md flex">
             evelyn is coding . . .
